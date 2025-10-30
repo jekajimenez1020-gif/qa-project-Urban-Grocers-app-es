f3390f7e1df69e747e4ad6e87bd35d6d6208e2da
Jessica Tatiana Jimenez Lopez <jekajimenez1020@gmail.com>
2025-10-30 10:27:52 -0500

    Correccion prueba 4

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-fc035c39-6f12-4349-8e1b-8c6385b691ca.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-6c0be10b-2e55-4f81-a8e5-54985c8a1171.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
